97d534509499d9b3582234b0144bfa94000def83
the-argus <ifm1110@rit.edu>
2021-09-30 14:38:46 -0400

    stripped slow physics code and overcomplicated polymorphism

--- run.py
+++ run.py
@@ -1,8 +1,4 @@
 import arcade
-from objects.player import Player
-from engine.core import Engine
-from engine.physics.object import PhysicsObject
-from engine.physics.equations import drag_force
 
 from CONSTANTS import (
                     SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE,
@@ -17,49 +13,6 @@ class GameWindow(arcade.Window):
 
     def setup(self):
         """ Set up the game and initialize the variables. """
-        self.eng = Engine()
-
-        # add the player, which has a special on_update so it's
-        # not made using the create_object function
-        self.player = Player("resources/square.png", 1)
-        # add it to the engine but don't have the engine update it
-        # we do that ourselves in on_update
-        self.eng.add_object(self.player, True, True)
-
-        #self.eng.create_object(True, True, arcade_sprite="\"resources/square.png\",1",
-        #                       PhysicsObject="x=50,y=50,static=True,has_collisions=False")
-
-        # make some walls
-        self.walls = []
-        for x_coord in range(int(SCREEN_WIDTH/DEFAULT_SIDE)):
-            args = "static=True,has_collisions=True,x=" + \
-                str(x_coord*DEFAULT_SIDE) + ",y=" + str(DEFAULT_SIDE)
-            self.walls.append(
-                self.eng.create_object(
-                    is_drawn=True, is_static=True, PhysicsObject=args)
-            )
-            args = "static=True,has_collisions=True,x=" + \
-                str(x_coord*DEFAULT_SIDE) + ",y=" + \
-                str(SCREEN_HEIGHT-DEFAULT_SIDE)
-            self.walls.append(
-                self.eng.create_object(
-                    is_drawn=True, is_static=True, PhysicsObject=args)
-            )
-
-        for y_coord in range(int(SCREEN_HEIGHT/DEFAULT_SIDE)):
-            args = "static=True,has_collisions=True,y=" + \
-                str(y_coord*DEFAULT_SIDE) + ",x=" + str(DEFAULT_SIDE)
-            self.walls.append(
-                self.eng.create_object(
-                    is_drawn=True, is_static=True, PhysicsObject=args)
-            )
-            args = "static=True,has_collisions=True,y=" + \
-                str(y_coord*DEFAULT_SIDE) + ",x=" + \
-                str(SCREEN_WIDTH-DEFAULT_SIDE)
-            self.walls.append(
-                self.eng.create_object(
-                    is_drawn=True, is_static=True, PhysicsObject=args)
-            )
 
         self.keys = {
                     "W": False,
@@ -70,7 +23,6 @@ class GameWindow(arcade.Window):
 
     def on_draw(self):
         arcade.start_render()
-        self.eng.draw_objects()
 
     def on_key_press(self, key, modifiers):
         if key == arcade.key.W:
@@ -115,12 +67,7 @@ class GameWindow(arcade.Window):
             self.keys["D"] = False
 
     def on_update(self, delta_time):
-        self.player.update(delta_time, self.keys)
-
-        #self.eng.physics_engine.apply_universal_force_func(
-        #   drag_force, in_class=PhysicsObject)
-
-        self.eng.update_objects(delta_time)
+        pass
 
 
 def main():
